b4e3a070237ab19dff587f3a5a3ef7de671de81d
Rustam Karimov <Karim-Sacaev@yandex.ru>
2023-04-10 22:08:46 +0500

    Доработан

--- spring.py
+++ spring.py
@@ -32,16 +32,18 @@ x0 = m*g/k
 xm = 1
 #Начальная Фаза
 φ0 = 0
+#Момент времени нарушения равновесия
+t_distr = 2.5
 
 fx = lambda t, A = xm, w0 =ω, fi0 = φ0: A * cos(w0*t + fi0)
 
 
-star_time = 0
+start_time = 0
 end_time = 10
 dt = 0.001
 
-time = np.arange(star_time, end_time, dt)
-mas_x = [fx(t) for t in time]
+time = np.arange(start_time, end_time, dt)
+mas_x = [x0 + (fx(t) if t > (start_time + t_distr) else 0) for t in time]
 
 fg, ax = plt.subplots(figsize=(7.5, 3.5), layout='constrained')
 
@@ -49,5 +51,6 @@ ax.plot(time, mas_x, label=f'x')
 ax.set_xlabel('Время, с')
 ax.set_ylabel('X, м')
 ax.set_title("График горманических колебаний пружиного маятника")
+ax.grid()
 
 plt.show()
